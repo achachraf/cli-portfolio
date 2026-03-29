34e2fbaea52b0f33f2434f4b4d17dbca7a3931e7
achraf <achrafachkari@hotmail.fr>
2026-03-29 19:11:39 +0200

    add standalone output

--- next.config.js
+++ next.config.js
@@ -1,22 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'gazelle.ihe.net',
-            port: '',
-            pathname: '/**',
-          },
-          {
-            protocol: 'https',
-            hostname: 'www.kereval.com',
-            port: '',
-            pathname: '/**',
-          },
-          
-        ],
-    },
+  images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'gazelle.ihe.net',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.kereval.com',
+        port: '',
+        pathname: '/**',
+      },
+
+    ],
+  },
+  output: "standalone"
 }
 
 module.exports = nextConfig
